0dc3a517d5ad010de7aa6ec98fff551feb168983
McKennon McMillian <mbmcmill@go.olemiss.edu>
2018-03-02 16:10:20 -0600

    Nothing works

--- BFS/bfs_simple.py
+++ BFS/bfs_simple.py
@@ -5,6 +5,7 @@ alpha = [x for x in string.ascii_lowercase[:26]]
 dict_four = open('../four_alpha.txt','r')
 dict_four = set([x[:-1] for x in dict_four])
 fringe = Queue(len(dict_four))
+visited = set()
 path_len = 0
 
 found = False
@@ -12,3 +13,7 @@ found = False
 start = 'cats'
 end = 'bear'
 
+visited.add(start)
+fringe.put(start)
+
+for i in range
